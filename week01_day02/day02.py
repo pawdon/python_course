022c2423a426f748c7478140d1fced701272e635
pawdon <pawdonapi@gmail.com>
2019-10-20 09:32:50 +0200

    Commit01

--- week01_day02/day02.py
+++ week01_day02/day02.py
@@ -13,3 +13,18 @@ def get_engines_list(minimum, maximum, step=0.1):
 
 
 engines = get_engines_list(minimum=1.0, maximum=2.3, step=0.2)  # [1.0, 1.2, ..., 2.2]
+
+
+def print_models(models):
+    for m in models:
+        print(m)
+
+
+def print_models_with_nr(models):
+    for i, m in enumerate(models):
+        print(f'{i}: {m}')
+
+
+# print_models(models)
+# print_models_with_nr(models)  # 0: Volvo
+print_model_color_unique(models, colors)  #  Volvo is red
