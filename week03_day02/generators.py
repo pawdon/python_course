f888d7536e41f350dba3e3c9c14d6d04280ccf24
pawdon <pawdonapi@gmail.com>
2019-11-17 10:45:46 +0100

    Generators

--- week03_day02/generators.py
+++ week03_day02/generators.py
@@ -5,19 +5,24 @@ def test_range():
 
 
 def my_generator(filename):
-    results = []
+    print('\tStart generator')
     with open(filename, 'r') as file:
+        print('\tOpened file')
         keys = file.readline().strip().split(';')
-        for line in file:
+        for i, line in enumerate(file):
+            print(f'\tStart single line {i}')
             values = line.strip().split(';')
             single_dict = {key: value for key, value in zip(keys, values)}
-            results.append(single_dict)
-    return results
+            yield single_dict  # taki maly return
+            print(f'\tEnd single line {i}')
 
 
 def test_my_generator():
-    results = my_generator(filename='box_since_2000.csv')
-    for single_dict in results:
+    results = my_generator(filename='box_since_2000.csv')  # jeszcze sie nic nie wykonalo
+    print('\tStart for loop', type(results))
+    for i, single_dict in enumerate(results):
+        if i >= 10:
+            break
         print(single_dict['Name'], single_dict['Height'], single_dict['Weight'])
 
 
