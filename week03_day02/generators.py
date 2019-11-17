0bbe1061e7c38fe16a40487e48afb3a520404110
pawdon <pawdonapi@gmail.com>
2019-11-17 10:39:13 +0100

    Generators

--- week03_day02/generators.py
+++ week03_day02/generators.py
@@ -4,17 +4,21 @@ def test_range():
     print(list(a))
 
 
-def my_generator():
-    with open('box_since_2000.csv', 'r') as file:
+def my_generator(filename):
+    results = []
+    with open(filename, 'r') as file:
         keys = file.readline().strip().split(';')
         for line in file:
             values = line.strip().split(';')
             single_dict = {key: value for key, value in zip(keys, values)}
-            print(single_dict)
+            results.append(single_dict)
+    return results
 
 
 def test_my_generator():
-    my_generator()
+    results = my_generator(filename='box_since_2000.csv')
+    for single_dict in results:
+        print(single_dict['Name'], single_dict['Height'], single_dict['Weight'])
 
 
 if __name__ == '__main__':
