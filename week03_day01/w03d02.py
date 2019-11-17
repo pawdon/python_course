23d3f7e84c93d3d221ba5b44b3d7023029b775dd
pawdon <pawdonapi@gmail.com>
2019-11-17 09:17:21 +0100

    Operator overload

--- week03_day01/w03d02.py
+++ week03_day01/w03d02.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
 
 
+# dataclass implements eg. __init__, __eq__
 @dataclass
-class Vector2D:
+class Vector2D_dataclass:
     # te pola wskoczyly na miejsce, gdzie wczesniej byly pola statyczne
     x: float
     y: float = 0
 
     def __post_init__(self):
         # wywolywane po __init__
-        print('Post init')
+        self.dist = self.abs()
 
     def __str__(self):
         return f'V({self.x}, {self.y})'
@@ -18,10 +19,36 @@ class Vector2D:
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
 
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'V({self.x}, {self.y})'
+
+    def abs(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    # operator == overload
+    def __eq__(self, other):
+        # return self.x == other.x and self.y == other.y
+        # getattr(self, k) <==> self.__dict__.get(k) <~=> self.__dict__[k]
+        self_keys = self.__dict__.keys()
+        other_keys = other.__dict__.keys()
+        if self_keys == other_keys:
+            return all([getattr(self, k) == getattr(other, k) for k in self_keys])
+        else:
+            return False
+
+
 def test_vector():
     v1 = Vector2D(3, 4)
     print(v1)
     print(v1.abs())
+    v2 = Vector2D(3, 4)
+    print(v1 == v2)
+    print(v1 + v2)
 
 
 if __name__ == '__main__':
