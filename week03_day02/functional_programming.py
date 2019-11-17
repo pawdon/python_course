ae5b27ab367f313b96c5ca6ad5ee7e87e88f6433
pawdon <pawdonapi@gmail.com>
2019-11-17 11:51:58 +0100

    Lambda

--- week03_day02/functional_programming.py
+++ week03_day02/functional_programming.py
@@ -11,11 +11,37 @@ def modify_x(x, func):
     return result
 
 
+def quadratic_equation(a, b, c):
+    def delta(a, b, c):
+        return b ** 2 - 4 * a * c
+
+    x1 = (-b + delta(a, b, c) ** 0.5) / 2
+    x2 = (-b - delta(a, b, c) ** 0.5) / 2
+
+    return x1, x2
+
+
 def test01():
     result = modify_x(7, double_x)
     print(result)
     result = modify_x(7, triple_x)
     print(result)
+
+    print(quadratic_equation(1, 0, -9))
+
+    modify_x_copy = modify_x
+    result = modify_x_copy(8, triple_x)
+    print(result)
+
+    # skladnia lambdy
+    # lambda moze byc tylko jednolinijkowa
+    # lambda <lista argumentow rozdzielana przecinkami>: <rezultat>
+    result = modify_x_copy(8, lambda x: 4 * x)
+    print(result)
+
+
+def test_lambda():
+    pass
 
 
 if __name__ == '__main__':
