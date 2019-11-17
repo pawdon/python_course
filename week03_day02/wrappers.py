4e764405c2e8848d38a57bbc65c46cb7be0b899e
pawdon <pawdonapi@gmail.com>
2019-11-17 15:12:43 +0100

    With statement

--- week03_day02/wrappers.py
+++ week03_day02/wrappers.py
@@ -1,5 +1,6 @@
 import functools
 import time
+from contextlib import contextmanager
 
 
 GLOBAL_X = 5
@@ -96,5 +97,65 @@ def test_wrapper():
     # long_fun()
 
 
+class ClassWith:
+    def __init__(self, name):
+        print('__init__')
+        self.name = name
+
+    def __enter__(self):
+        print('Inside enter', self.name)
+        return 7
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Inside exit', self.name)
+
+
+def test_with():
+    print('Start')
+    obj = ClassWith('Adam')
+    with obj as x:
+        print('Inside with', x)
+    print('Stop')
+
+
+class ClassWith2:
+    def __init__(self, name):
+        print('__init__')
+        self.name = name
+
+    def __enter__(self):
+        print('Inside enter', self.name)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Inside exit', self.name)
+
+
+def test_with2():
+    print('Start')
+    with ClassWith2('Adam') as x:
+        print('Inside with', x.name)
+    print('Stop')
+
+
+@contextmanager
+def context_funct(name):
+    print('Begin', name)
+    try:
+        print('Try')
+        yield 5
+        print('After yield')
+    finally:
+        # jesli trzeba cos zamknac to tu
+        print('End')
+
+
+def test_with3():
+    print('Start')
+    with context_funct('Adam') as x:
+        print('Inside with', x)
+    print('Stop')
+
+
 if __name__ == '__main__':
-    test_wrapper()
+    test_with3()
