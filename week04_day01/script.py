0cfc4abf50a370acb4333d9fe1023cd79163a07c
pawdon <pawdonapi@gmail.com>
2019-11-30 13:41:10 +0100

    Argparse

--- week04_day01/script.py
+++ week04_day01/script.py
@@ -22,9 +22,23 @@ def fun_arg03():
     parser.add_argument('xyz', help='a help')
     parser.add_argument('-a', help='a help')
     parser.add_argument('-b', help='b help', required=True, type=int)
-    parser.add_argument('-c', help='c help', default=1.5, type=float)
+    parser.add_argument('-c', '--my_argument', help='c help', default=1.5, type=float)
     args = parser.parse_args()
     print(args)
+
+
+def fun_arg04():
+    a = float(sys.argv[1])
+    b = float(sys.argv[2])
+    print(a + b)
+
+
+def fun_arg05():
+    parser = argparse.ArgumentParser('Description of my function')
+    parser.add_argument('a', help='a help', type=float)
+    parser.add_argument('b', help='b help', type=float)
+    args = parser.parse_args()
+    print(args.a + args.b)
 
 
 """
@@ -33,4 +47,4 @@ python script.py 2.5 3.5  # 6; zrob 2 wersje (z uzyciem sys.argv i z uzyciem arg
 
 
 if __name__ == '__main__':
-    fun_arg03()
+    fun_arg05()
