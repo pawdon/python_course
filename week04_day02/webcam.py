c30bc9fbba25a4b2b965a56fa9c687a71b3f9d95
pawdon <pawdonapi@gmail.com>
2019-12-01 12:19:26 +0100

    cv2

--- week04_day02/webcam.py
+++ week04_day02/webcam.py
@@ -3,32 +3,22 @@ import cv2
 import time
 
 
+def show_image():
+    img: np.ndarray = cv2.imread('Lenna.png')  # img jest juz np.ndarray
+    print(type(img), np.shape(img), img.dtype)
+    cv2.imshow('my image', img)  # pokazanie okna o nazwie 'my image' z tym obrazem
+    cv2.waitKey(0)  # czekaj na nacisniecie jakiegos klawisza przez x ms; x <=0 oznacza nieskonczenie dlugo
+    cv2.destroyAllWindows()  # zniszcz okna
+
+
 def show_webcam():
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(0)  # jesli wpiszemy nr, to oznacza to kamerke, jesli nazwe pliku, to plik
     while True:
-        ret_val, img = cam.read()
-        shape = np.shape(img)
-
-        stime = time.time()
-        """
-        for i in range(shape[0]):
-            for j in range(shape[1]):
-                for k in range(shape[2]):
-                    img[i, j, k] *= 2
-                    if img[i, j, k] > 255:
-                        img[i, j, k] = 255
-        # """
-
-        # """
-        img *= 2
-        img = np.where(img > 255, 255, img)  
-        # """
-
-        print('Time', time.time() - stime, 's')
-
-        cv2.imshow('my webcam', img)
-        if cv2.waitKey(1) == ord('q'):
-            break  # esc to quit
+        ret_val, img = cam.read()  # ret_val to bool, ktory mowi, czy sie udalo
+        if ret_val:
+            cv2.imshow('my webcam', img)
+            if cv2.waitKey(1) == ord('q'):  # waitKey zwraca kod przycisku, ktory zostal nacisniety
+                break  # esc to quit
     cv2.destroyAllWindows()
 
 
