4f680592b0863734dd830425fedb3fa6ea9067da
pawdon <pawdonapi@gmail.com>
2019-12-07 09:13:58 +0100

    Egzamin zad 1

--- week05_day01/egz_zad1.py
+++ week05_day01/egz_zad1.py
@@ -1,7 +1,7 @@
 class Punkt:
-    def __init__(self):
-        self.x: float = 0
-        self.y: float = 0
+    def __init__(self, x, y):
+        self.x: float = x
+        self.y: float = y
 
 
 class Ksztalt2D:
@@ -17,13 +17,13 @@ class Kolo(Ksztalt2D):
     def __init__(self):
         self.Sr: Punkt = None
         self.r: float = 0
-        
+
 
 class Trojkat(Ksztalt2D):
-    def __init__(self):
-        self.A: Punkt = None
-        self.B: Punkt = None
-        self.C: Punkt = None
+    def __init__(self, Ax: float, Ay: float, Bx: float, By: float, Cx: float, Cy: float):
+        self.A: Punkt = Punkt(Ax, Ay)
+        self.B: Punkt = Punkt(Bx, By)
+        self.C: Punkt = Punkt(Cx, Cy)
 
 
 
