b45484dfa834cefd439c7cf92203b006ffdcbc9e
pawdon <pawdonapi@gmail.com>
2019-12-07 12:23:15 +0100

    Egzamin zad 2

--- week05_day01/egz_zad2.py
+++ week05_day01/egz_zad2.py
@@ -27,18 +27,46 @@ def histogram(text: str):
                 letters_hist[letter] = 1
             """
             letters_hist[letter] = letters_hist.get(letter, 0) + 1
-    print(letters_hist)
+    # print(letters_hist)
     # Zalozmy, ze histogram ma byc w kolejnosci 0,1,2,...,9,a,b,c,...,z
     ordered_letters = get_ordered_letters()
-    print(ordered_letters)
+    # print(ordered_letters)
     counts = [str(letters_hist.get(key, 0)) for key in ordered_letters]
-    print(counts)
+    # print(counts)
 
     with open('hist.txt', 'w') as file:
         file.write(';'.join(counts))
+
+    return letters_hist
+
+
+def letters_replace(text):
+    letters_hist = histogram(text)
+    print(letters_hist)
+    count_list = [{'letter': key, 'count': val} for key, val in letters_hist.items()]
+    print(count_list)
+    count_list.sort(key=lambda x: x['count'], reverse=True)
+    print(count_list)
+
+    letter1 = count_list[0]['letter']
+    letter2 = count_list[1]['letter']
+    print(letter1, letter2)
+
+    text_as_list = list(text)
+    for i in range(len(text_as_list)):
+        if text_as_list[i] == letter1:
+            text_as_list[i] = letter2
+        elif text_as_list[i] == letter2:
+            text_as_list[i] = letter1
+
+    text = ''.join(text_as_list)
+    print(text)
+
+    with open('zamiana.txt', 'w') as file:
+        file.write(text)
 
 
 if __name__ == '__main__':
     text = read_file('literki.txt')
     print(text)
-    histogram(text)
+    letters_replace(text)
