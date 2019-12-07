b14ec289ded2f6dd3cadd65512c1da2e56cc13a4
pawdon <pawdonapi@gmail.com>
2019-12-07 14:15:49 +0100

    Pytest fixture

--- week05_day01/test_examples.py
+++ week05_day01/test_examples.py
@@ -49,10 +49,20 @@ def test_01():
     assert 2 == 2, 'Opcjonalny opis'
 
 
+def more_values_to_test(filename):
+    values = []
+    with open(filename, 'r') as f:
+        for line in f:
+            split = line.strip().split(',')
+            val = int(split[0]), int(split[1])
+            values.append(val)
+    return values
+
+
 # @pytest.mark.parametrize przyjmuje 2 argumenty:
 # stringa z nazwami argumentow rozdzielanymi przecinkami
 # liste tupli z wartosciami
-@pytest.mark.parametrize('arg, val', [(0, 1), (1, 1), (2, 2), (3, 6), (4, 24)])
+@pytest.mark.parametrize('arg, val', [(0, 1), (1, 1), (2, 2), (3, 6), (4, 24)] + more_values_to_test('test_silnia_values.csv'))
 def test_silnia_correct(arg, val):
     assert silnia(arg) == val
 
@@ -61,6 +71,24 @@ def test_silnia_incorrect():
     # match jest opcjonalne
     with pytest.raises(ValueError, match='n should be >= 0'):
         silnia(-1)
+
+
+# fixture sluzy do przygotowania danych do testow
+@pytest.fixture(scope='session')
+def prepare_data():
+    # jakies laczenie sie z baza danych itp
+    # generalnie duze przygotowanie obiektu
+    print('\nSTART_PREPARE_DATA')
+    yield 'Volvo'
+    print('\nSTOP_PREPARE_DATA')
+
+
+def test_prepared_data(prepare_data):
+    assert prepare_data in ['Volvo', 'Skoda', 'BMW', 'Toyota', 'Audi']
+
+
+def test_prepared_data2(prepare_data):
+    assert len(prepare_data) < 100
 
 
 if __name__ == '__main__':
